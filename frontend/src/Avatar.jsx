dd514829da23e87c0a0df754ba1a067eb2720373
Pat Flaherty <patflaherty321@gmail.com>
2025-07-19 15:49:12 -0700

    v5.4.1: Fix missing useState import in Avatar.jsx

--- frontend/src/Avatar.jsx
+++ frontend/src/Avatar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
 
 function Avatar({ 
